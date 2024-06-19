7b9eb37d2d83bf0765013a69c81cd5ade9672f4b
AppleOngPS <s10262488@connect.np.edu.sg>
2024-06-19 21:17:54 +0800

    Update of sign in and login page

--- public/js/login.js
+++ public/js/login.js
@@ -1,38 +1,33 @@
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('LoginForm').addEventListener('submit', async (event) => {
+    event.preventDefault(); // Prevent default form submission
 
+    const loginForm = event.target;
+    const formData = {
+      name: loginForm.querySelector('#exampleInputName').value,
+      password: loginForm.querySelector('#exampleInputPassword').value
+    };
 
-document.getElementById('LoginForm').addEventListener('submit', async (event) => {
-  event.preventDefault(); // Prevent default form submission
-  
-  const signupForm = event.target;
-  const formData = {
-    name: signupForm.querySelector('#exampleInputName').value,
-    password: signupForm.querySelector('#exampleInputPassword').value,
-  };
-  
-  try {
-    const response = await fetch(`http://localhost:3001/users/search?name=${formData.name}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
+    try {
+      const queryString = `?name=${encodeURIComponent(formData.name)}&password=${encodeURIComponent(formData.password)}`;
+      const response = await fetch(`http://localhost:3000/login${queryString}`, {
+        method: 'GET', // Use GET method for login
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+      if (!response.ok) {
+        throw new Error('Login failed');
       }
-    });
-    
-    if (!response.ok) {
-      throw new Error('Failed to retrieve user account');
+
+      const loggedInUser = await response.json();
+      console.log('Logged in user:', loggedInUser);
+      // Redirect or perform actions upon successful login
+      window.location.href = 'profile.html'; // Example redirect to profile page
+    } catch (error) {
+      console.error('Error logging in:', error);
+      // Handle error: show error message to user
     }
-    
-    const users = await response.json();
-    if (users.length > 0) {
-      const user = users[0]; // Assuming the first user found
-      console.log('Login successfully. Account Name:', user.name);
-      // Redirect to the login page or handle success as needed
-       window.location.href = '../html/profile.html';
-    } else {
-      console.error('User not found');
-      // Handle case where user was not found
-    }
-  } catch (error) {
-    console.error('Error logging in:', error);
-    // Handle error: show error message to user
-  }
+  });
 });
