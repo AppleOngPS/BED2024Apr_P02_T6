e8451b139d8fa2ed5c2e3330efbc249f2ffafc96
Ahmed <S10258532@connect.np.edu.sg>
2024-07-21 16:56:12 +0800

    .

--- dbConfig.js
+++ dbConfig.js
@@ -13,26 +13,26 @@ const sql = require("mssql");
 // };
 
 //ahmed
-// module.exports = {
-//   user: "booksapi_user",
-//   password: "27D988332n07",
-//   server: "localhost",
-//   database: "BED_ass1",
-//   trustServerCertificate: true,
-//   options: {
-//     port: 1433, // Default SQL Server port
-//     connectionTimeout: 60000, // Connection timeout in milliseconds
-//   },
-// };
-
 module.exports = {
-  user: "BED_user", // Replace with your SQL Server login username
-  password: "hello123", // Replace with your SQL Server login password
+  user: "booksapi_user",
+  password: "27D988332n07",
   server: "localhost",
-  database: "bed_Asg1",
+  database: "BED_ass1",
   trustServerCertificate: true,
   options: {
     port: 1433, // Default SQL Server port
     connectionTimeout: 60000, // Connection timeout in milliseconds
   },
 };
+
+// module.exports = {
+//   user: "BED_user", // Replace with your SQL Server login username
+//   password: "hello123", // Replace with your SQL Server login password
+//   server: "localhost",
+//   database: "bed_Asg1",
+//   trustServerCertificate: true,
+//   options: {
+//     port: 1433, // Default SQL Server port
+//     connectionTimeout: 60000, // Connection timeout in milliseconds
+//   },
+// };
