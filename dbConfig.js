557aa18afdafd48f42bcf7bc71ed1186529dd466
Ahmed <S10258532@connect.np.edu.sg>
2024-07-24 11:27:55 +0800

    did some final changes to the recipe page

--- dbConfig.js
+++ dbConfig.js
@@ -25,14 +25,14 @@ const sql = require("mssql");
 //   },
 // };
 
-  module.exports = {
-    user: "BED_user", // Replace with your SQL Server login username
-   password: "hello123", // Replace with your SQL Server login password
-    server: "localhost",
-    database: "bed_Asg1",
-   trustServerCertificate: true,
-   options: {
-     port: 1433, // Default SQL Server port
-      connectionTimeout: 60000, // Connection timeout in milliseconds
-    },
-  };
+module.exports = {
+  user: "BED_user", // Replace with your SQL Server login username
+  password: "hello123", // Replace with your SQL Server login password
+  server: "localhost",
+  database: "bed_Asg1",
+  trustServerCertificate: true,
+  options: {
+    port: 1433, // Default SQL Server port
+    connectionTimeout: 60000, // Connection timeout in milliseconds
+  },
+};
